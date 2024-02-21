658f3780db5328171ef69a1c22234bf90860b942
Jakub Adamczyk <jakub.adamczyk@oponeo.pl>
2024-02-21 16:46:05 +0100

    lista punktów

--- dragon-multiply-quest/src/component/game/game.component.ts
+++ dragon-multiply-quest/src/component/game/game.component.ts
@@ -11,6 +11,11 @@ export enum GameState {
   Success
 }
 
+export interface GameResult {
+  task: string,
+  points: number
+}
+
 @Component({
   selector: 'dramul-game',
   standalone: true,
@@ -22,16 +27,17 @@ export class GameComponent {
   public buttons: number[] = [1,2,3,4,5,6,7,8,9,0];
   private taskQuantity: number = 7;
   private errorPunishment: number = 3;
-  
+
   public state: GameState = GameState.Begining;
-  public taskIndex: number = 0; 
+  public taskIndex: number = 0;
   public userResult: string = '';
   private rightAnswer?: number;
   public deck: Task[];
   public GameState = GameState;
   private timestampId?: ReturnType<typeof setTimeout>;
-  private timer: number = 0;  
+  private timer: number = 0;
   private errors: number = 0;
+  public gameResultTable: GameResult[] = [];
 
   private _rightsService = inject(RightsService)
 
@@ -40,39 +46,52 @@ export class GameComponent {
     this.timer = new Date().getTime();
   }
 
-  public startGame(): void {    
+  public startGame(): void {
     this.startTask();
-  }  
+    this.gameResultTable = [];
+  }
 
   private finishGame(): void {
-    this.taskIndex = 0;      
+    this.taskIndex = 0;
     this.state = GameState.Begining;
   }
 
-  private startTask(): void {    
+  private startTask(): void {
     this.state = GameState.Runing;
     const task = this.deck[this.taskIndex];
     this.rightAnswer = task.x * task.y;
     this.timer = new Date().getTime();
   }
 
-  private finishTask(): void {    
+  private finishTask(): void {
     this.timestampId = setTimeout(()=>{
       const duration = Math.floor((new Date().getTime() - this.timer) / 1000);
       const punishment = this.errors * this.errorPunishment;
-      let points = 10 - duration - punishment + (duration !== 0 ? 1 : 0);
-      if(points === 0 && punishment === 0) points = 1;
 
-      console.log(duration, punishment, points);
+      let points = 10 - duration - punishment + this.rightAnswer!.toString().length;
+      if(points > 10){
+         points = 10;
+      } else if (points <= 0 && punishment === 0){
+         points = 1;
+      } else if(points < 0){
+        points = 0;
+      }
 
-      this.errors = 0; 
-      this.userResult = '';  
+      const task = this.deck[this.taskIndex];
+
+      this.gameResultTable.unshift({
+        task: `${task.x} x ${task.x} = ${this.rightAnswer}`,
+        points: points
+      })
+
+      this.errors = 0;
+      this.userResult = '';
       if(this.taskIndex === this.taskQuantity - 1 || this.taskIndex === this.deck.length -1){
         this.finishGame();
       } else {
-        this.taskIndex = this.taskIndex + 1;     
+        this.taskIndex = this.taskIndex + 1;
         this.startTask();
-      }      
+      }
     }, 500);
   }
 
@@ -88,25 +107,25 @@ export class GameComponent {
     } else {
       this.state = GameState.Runing;
     }
-  }  
+  }
 
   public keyRemove():void {
     if(this.state === GameState.Error || this.state === GameState.Runing){
-      const length = this.userResult.length; 
+      const length = this.userResult.length;
       if(length){
         this.userResult = this.userResult.slice(0,length-1);
-      }    
+      }
     }
   }
 
-  public keyNumber(value: number):void { 
+  public keyNumber(value: number):void {
     if(this.state === GameState.Error || this.state === GameState.Runing){
       if(this.state === GameState.Error){
         this.userResult = value.toString();
       } else if (this.state === GameState.Runing){
         this.userResult = '' + this.userResult + value;
-      }     
+      }
       this.checkResult();
-    }    
-  }  
+    }
+  }
 }
